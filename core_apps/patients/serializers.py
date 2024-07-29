8aef4119f94885ca7038819f420dada22ff5a8dc
JCalleros <jcalleros1009@gmail.com>
2024-07-28 18:04:38 -0700

    general improvements

--- core_apps/patients/serializers.py
+++ core_apps/patients/serializers.py
@@ -1,15 +1,26 @@
 from rest_framework import serializers
 from core_apps.owners.models import Owner
 from .models import Patient
-
+from core_apps.owners.serializers import OwnerSerializer
 
 
 class PatientSerializer(serializers.ModelSerializer):
-    owner_name = serializers.CharField(source='owner.name', read_only=True)
-    owner_phone_number = serializers.CharField(source='owner.phone_number', read_only=True)
-    
-    
+    owner = OwnerSerializer(read_only=True)
+
     class Meta:
         model = Patient
-        fields = ["id", "name", "species", "gender", "owner", "owner_name", "owner_phone_number", "created_at"]
+        fields = ['id', 'name', 'species', 'gender', 'breed', 'age_years', 'age_months', 'age_weeks', 'color', 'photo', 'slug', 'owner']
 
+
+    def create(self, validated_data):
+        owner_uuid = self.initial_data.get('owner')
+        owner = Owner.objects.get(id=owner_uuid)
+        patient = Patient.objects.create(**validated_data, owner=owner)
+        return patient
+    
+    def update(self, instance, validated_data):
+        owner_uuid = validated_data.pop('owner', None)
+        if owner_uuid:
+            owner = Owner.objects.get(id=owner_uuid)
+            instance.owner = owner
+        return super().update(instance, validated_data)
