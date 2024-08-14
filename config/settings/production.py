87674d332f2bfc21ba486410f154fc4d4810d6d6
JCalleros <jcalleros1009@gmail.com>
2024-08-14 13:32:13 -0700

    Add allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ SECRET_KEY = getenv("DJANGO_SECRET_KEY")
 
 ADMIN_URL = getenv("DJANGO_ADMIN_URL")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["vetmanagementapi.onrender.com"]
 
 ADMINS=[("Jorge Calleros", "calleros.dev@gmail.com"),]
 
